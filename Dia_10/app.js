c1832ddeea8b949f8026e1c36e72df9c9642b395
arnoldbgm <arnold.gallegosm@gmail.com>
2025-05-12 21:36:18 -0500

    JS DOM

--- Dia_10/app.js
+++ Dia_10/app.js
@@ -16,7 +16,7 @@ let tituloDOM = document.querySelector("#titulo")
 
 /*Queremos capturar el valor de un input */
 
-let input1 = document.getElementById("num1") 
+let input1 = document.getElementById("num1")
 let input2 = document.getElementById("num2")
 // Capturo el valor del input
 // El valor de los inputs siempre llega en String
@@ -25,14 +25,54 @@ let input2 = document.getElementById("num2")
 // 1. Capturar la etiqueta 
 const btnSumar = document.getElementById("sumar")
 
+
 // 2. Añadimos el addEventListener y el evento
 
 let txtResult = document.getElementById("resultado")
 
-btnSumar.addEventListener("click", function(){
+const btnMultiplicar = document.getElementById("multiplicar")
+
+btnSumar.addEventListener("click", function () {
    // Para hacer la suma y que se muestre en un alert
    let val1 = Number(input1.value)
    let val2 = Number(input2.value)
    let total = val1 + val2
    txtResult.innerText = total
+})
+
+
+/*
+   🍏 RETO 01
+   Dar funcionamiento a los botones de: 
+   -resta
+   -multiplicacion
+   -division
+   -Limpiar
+
+    🌟 RETO EXTRA:
+   Si el resultado es negativo que se muestre en rojo
+   Si el resultado es positivo que se muestre en verde
+   Validar cuando es la division por 0 que me lanze un 
+       alert
+   Validar que los inputs no esten vacios
+*/
+
+btnMultiplicar.addEventListener("click", function () {
+   // Validacion de inputs vacios
+   if (input1.value === "" || input2.value === ""){
+      alert("Los inputs no pueden estar vacios")
+      return;
+   }
+   
+   let val1 = Number(input1.value)
+   let val2 = Number(input2.value)
+   let total = val1 * val2;
+   txtResult.innerText = total
+   // Si el resultado es negativo rojo
+   // Si es positivo verde
+   if (total < 0){
+      txtResult.style.color = "red";
+   } else{
+      txtResult.style.color = "green";
+   }
 })
