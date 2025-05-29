b6c8dfc1f3c5d19de1c12b2a09fb062246941224
Arnold Gallegos <arnold.gallegosm@gmail.com>
2025-05-28 20:18:22 -0500

    App State store

--- Dia_14/js/app.js
+++ Dia_14/js/app.js
@@ -1,4 +1,4 @@
-const plantillas = []
+let plantillas = []
 
 /*
    Quiero crear una instancia de Template
@@ -8,7 +8,38 @@ const plantillas = []
 
 /*
    1. Capturar los elementos con los querySelector
-    1.1 Validacion de los valores try catch
    2. Crear la instancia => new Template(tituloDOM, mensajeDOM, hasDOM)
    3. Lo inserto a mi arreglo plantillas.
 */
+
+
+const btnSave = document.querySelector("#save-template-btn")
+
+btnSave.addEventListener("click", function () {
+
+   // 💚  Capturar los elements e insertarlos a mi arreglo
+   const inputTitle = document.querySelector("#template-title").value.trim()
+   const inputHashtag = document.querySelector("#template-hashtag").value.trim()
+   const inputMessage = document.querySelector("#template-message").value.trim()
+
+   const newTemplate = new Template(inputTitle, inputMessage, inputHashtag)
+
+   // VAmos a insertar las plantillas
+   // 1. Push
+   // plantillas.push(newTemplate)
+
+   // 2. Spread operator
+   plantillas = [...plantillas, newTemplate]
+
+   // 💚 Renderizar el arreglo dentro de mi contenedor div
+   const containerTemplate = document.querySelector("#templates-container")
+   // Limpiar el contenedor
+   containerTemplate.innerHTML = ""
+   // Vamos a renderizarlo
+   plantillas.forEach((elmt) => {
+      
+   })
+
+
+
+})
